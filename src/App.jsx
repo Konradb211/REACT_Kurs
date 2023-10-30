3ce958ceca3f231a9093167cb24742a537616fea
Konrad Baś <konradbas@MacBook-Air-Konrad.local>
2023-10-30 20:06:59 +0100

    half of module - DONE

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
-import { useState } from "react"
 import "./App.css"
 
 function App() {
-	const [click, setClick] = useState(0)
-	const handleButtonClick = () => {
-		setClick(prevClick => prevClick + 1)
-	}
+	const paragraphElement = (
+		<>
+			<h2>Heading</h2>
+			<p>
+				Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis
+				facere consectetur consequatur a perspiciatis beatae dolores maiores
+				officia in, vel, molestias, aliquam repellat itaque quam!
+			</p>
+		</>
+	)
 	return (
 		<>
-			<h1>{click}</h1>
-			<button onClick={handleButtonClick}>Kliknij na mnie</button>
-			<p>
-				Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dignissimos
-				unde neque ipsam eligendi numquam natus, dolor iure ab facere
-				consequuntur praesentium eaque nisi architecto harum autem ea ut
-				voluptate sunt.
-			</p>
+			{paragraphElement} {paragraphElement} {paragraphElement}{" "}
+			{paragraphElement} {paragraphElement} {paragraphElement}{" "}
 		</>
 	)
 }
